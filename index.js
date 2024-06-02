0902a66a0652735834e791d8e61c7ffe70f0f711
github-cloudlabsuser-697 <167424241+github-cloudlabsuser-697@users.noreply.github.com>
2024-06-02 09:13:36 +0000

    Second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const data = [
+    [
+        { name: 'John', age: 25 },
+        { name: 'Jane', age: 30 }
+    ],
+    [
+        { name: 'Bob', age: 40 }
+    ]
+];
+
+
+// Using flatMap
+const names = data.flatMap(arr => arr.map(obj => obj.name));
+console.log(names);
